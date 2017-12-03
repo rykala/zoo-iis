4609a396c6fc92c7c6aa971527063d548dd84d55
Kryštof Rykala <rykala.k@gmail.com>
2017-12-03 12:31:28 +0100

    menu

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -45,22 +45,18 @@
 
 <header>
     <nav class="navbar navbar-toggleable-md navbar-dark bg-dark">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+        <div class="navbar-nav">
+            <a class="navbar-brand" href="{{ url('/home') }}">ZOO IIS</a>
+            <a class="nav-item nav-link" style="display: inline-block;" href="{{ url('/osetrovatele' . '/' . Auth::user()->idOsetrovatele) }}">Profil ({{ Auth::user()->email }})</a> 
+        </div>
+      <button class="navbar-toggler navbar-toggler-right ml-auto justify-content-end" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <a class="navbar-brand" href="{{ url('/home') }}">ZOO IIS</a>
+
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/home') }}">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/osetrovatele' . '/' . Auth::user()->idOsetrovatele) }}">Profil ({{ Auth::user()->email }})</a>
-            </li>
-
-            <li class="divider"></li>
-
             <li class="nav-item">
                 <a href="{{url('/osetrovatele')}}" class="nav-link">Ošetřovatelé</a>
             </li>
